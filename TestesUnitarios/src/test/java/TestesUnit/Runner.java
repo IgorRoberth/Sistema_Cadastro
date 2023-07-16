b7c7c315fb7dc2f8a7b3d86233df5fa735c54f6f
Igor Roberth <igorroberthm@gmail.com>
2023-07-16 14:44:28 -0300

    Adicionado teste unitário que valida se no sistema contém cliente com o mesmo e-mail cadastrado

---
+++ TestesUnitarios/src/test/java/TestesUnit/Runner.java
@@ -0,0 +1,13 @@
+package TestesUnit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+
+	SistemaCadastroTest.class
+})
+public class Runner {
+}
--- TestesUnitarios/src/test/java/TestesUnit/SistemaCadastroTest.java
+++ TestesUnitarios/src/test/java/TestesUnit/SistemaCadastroTest.java
@@ -4,15 +4,21 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.List;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.mockito.Mockito;
 import sistema.SistemaCadastro;
+import sistema.SistemaCadastro.CadastroExistenteException;
+import sistema.SistemaCadastro.CadastroExistenteVerifier;
 import sistema.SistemaCadastro.CadastroInvalidoException;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SistemaCadastroTest {
-
+	
 	@SuppressWarnings("unused")
 	@Test
-	public void testeCadastroConcluidoComSucesso() throws CadastroInvalidoException {
+	public void teste01CadastroConcluidoComSucesso() throws CadastroInvalidoException {
 
 		String nome = "Roberto";
 		String sobrenome = "Silveira";
@@ -31,7 +37,7 @@ public class SistemaCadastroTest {
 	}
 
 	@Test
-	public void testeCampoNomeVazio() {
+	public void teste02CampoNomeVazio() {
 
 		String nome = "";
 		String sobrenome = "Brum";
@@ -51,7 +57,7 @@ public class SistemaCadastroTest {
 	}
 
 	@Test
-	public void testeCampoNomeComCaracteresInvalido() {
+	public void teste03CampoNomeComCaracteresInvalido() {
 
 		String nome = "Alin$";
 		String sobrenome = "Muniz";
@@ -71,7 +77,7 @@ public class SistemaCadastroTest {
 	}
 
 	@Test
-	public void testeCampoSobrenomeVazio() {
+	public void teste04CampoSobrenomeVazio() {
 
 		String nome = "Hélio";
 		String sobrenome = "";
@@ -91,7 +97,7 @@ public class SistemaCadastroTest {
 	}
 
 	@Test
-	public void testeCampoCEPComLetra() {
+	public void teste05CampoCEPComLetra() {
 
 		String nome = "Aline";
 		String sobrenome = "Muniz";
@@ -108,5 +114,40 @@ public class SistemaCadastroTest {
 			List<String> mensagensErro = e.getMensagensErro();
 			Assert.assertEquals("CEP inválido: caracteres inválidos.", mensagensErro.get(0));
 		}
+	}
+	
+	@SuppressWarnings("unused")
+	@Test
+	public void teste06CadastroUsuarioExistente() {
+	    String nome = "Roberto";
+	    String sobrenome = "Silveira";
+	    String cidade = "Santa Cruz do Sul";
+	    String cep = "61701-292";
+	    String endereco = "Viela Vitor Silva";
+	    String idade = "28";
+	    String email = "antonio.caseira@gmail.com";
+
+	    // Cria um mock do CadastroExistenteVerifier
+	    CadastroExistenteVerifier cadastroExistenteVerifier = Mockito.mock(CadastroExistenteVerifier.class);
+	    
+	    // Define o comportamento do mock para retornar true, indicando que o usuário já está cadastrado
+	    Mockito.when(cadastroExistenteVerifier.verificarCadastroExistente(email)).thenReturn(true);
+
+	    // Cria uma instância de SistemaCadastro usando o Builder
+	    SistemaCadastro sistemaCadastro = SistemaCadastro.builder()
+	            .cadastroExistenteVerifier(cadastroExistenteVerifier)
+	            .build();
+
+	    // Executa o teste e captura a saída do console
+	    ByteArrayOutputStream consoleOutput = new ByteArrayOutputStream();
+	    PrintStream originalOut = System.out;
+	    System.setOut(new PrintStream(consoleOutput));
+	    try {
+	        sistemaCadastro.cadastrarUsuario(email);
+	    } catch (CadastroExistenteException e) {
+	        // Verifica se a mensagem de erro é exibida corretamente
+	        Assert.assertEquals("Cliente já está cadastrado no sistema.", e.getMessage());
+	    }
+	    System.setOut(originalOut);
 	}
 }
