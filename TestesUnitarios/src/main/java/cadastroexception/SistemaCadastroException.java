795b75c9755c41d599e4f06e53b002b9867e72c7
Igor Roberth <igorroberthm@gmail.com>
2023-07-18 20:50:17 -0300

    Continua a mesma configuração

--- TestesUnitarios/src/main/java/cadastroexception/SistemaCadastroException.java
+++ TestesUnitarios/src/main/java/cadastroexception/SistemaCadastroException.java
@@ -7,11 +7,11 @@ public class SistemaCadastroException extends Exception {
 
 	public List<String> mensagensErro;
 
-	    public SistemaCadastroException(List<String> mensagensErro) {
-	        this.mensagensErro = mensagensErro;
-	    }
+	public SistemaCadastroException(List<String> mensagensErro) {
+		this.mensagensErro = mensagensErro;
+	}
 
-	    public List<String> getMensagensErro() {
-	        return mensagensErro;
-	    }	
+	public List<String> getMensagensErro() {
+		return mensagensErro;
+	}
 }
