082c307f42ed80c93508b8ee04bb94234046e1e1
Igor Roberth <igorroberthm@gmail.com>
2023-07-20 16:44:35 -0300

    Nenhuma alteração complexa

--- TestesUnitarios/src/main/java/verificarcadastromock/VerificarCadastroMock.java
+++ TestesUnitarios/src/main/java/verificarcadastromock/VerificarCadastroMock.java
@@ -6,12 +6,12 @@ public class VerificarCadastroMock implements VerificarCadastro {
     private boolean cadastroExistente;
 
     @Override
-    public boolean verificarCadastroExistente(String email) {
-        return cadastroExistente;
-    }
-
-    @Override
     public void setCadastroExistente(boolean cadastroExistente) {
         this.cadastroExistente = cadastroExistente;
+    }
+    
+    @Override
+    public boolean verificarCadastroExistente(String email) {
+        return cadastroExistente;
     }
 }
