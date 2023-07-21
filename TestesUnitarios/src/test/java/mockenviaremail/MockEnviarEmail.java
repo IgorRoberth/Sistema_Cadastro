fa8f4a33df176ba1a52c715399e2950ea1c40372
Igor Roberth <igorroberthm@gmail.com>
2023-07-21 01:59:07 -0300

    Criada nova page com objeto mock, com objetivo de validar o teste de envio de mensagem

---
+++ TestesUnitarios/src/test/java/mockenviaremail/MockEnviarEmail.java
@@ -0,0 +1,9 @@
+package mockenviaremail;
+
+import enviaremail.EnviarEmail;
+
+public class MockEnviarEmail implements EnviarEmail {
+	@Override
+	public void enviarEmail(String destinatario, String assunto, String corpo) {
+	}
+}
